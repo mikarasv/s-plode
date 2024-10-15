3c582f4dfed858323ccecb617625e53c3409eb06
sara <victoriasarakim.sk@gmail.com>
2024-10-15 17:56:39 -0300

    fix: entry file and git ignore

--- entry.sh
+++ entry.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-# docker run --rm -it --volume ./Samples/Integers/flow_switch:/home/klee/sample --ulimit='stack=-1:-1' klee/klee:3.0
+set -eu
 
 # Buscar el archivo que termine en splode.c
 archivo=$(find /home/klee/sample/ -type f -name "*splode.c")
 
 if [ -n "$archivo" ]; then
-    clang -I klee_src/ -emit-llvm -c -g -fsanitize=integer "$archivo" -o /home/klee/sample/code.bc
+    clang -I klee_src/ -debug-mode -emit-llvm -c -g -Wno-macro-redefined -fsanitize=signed-integer-overflow -ferror-limit=1000 "$archivo" -o /home/klee/sample/code.bc
     klee /home/klee/sample/code.bc
     if [ -n "$(find /home/klee/sample/klee-last/ -type f -name "*.err")" ]; then
         echo "SPLODED!!"
